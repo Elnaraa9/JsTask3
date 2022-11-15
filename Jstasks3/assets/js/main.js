41d64634d2935c7fa71cb008b4aa516dc06465ef
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-11-15 15:33:04 +0400

    Add files via upload

---
+++ Jstasks3/assets/js/main.js
@@ -0,0 +1,23 @@
+// 1st Task
+
+// let array = [2, 29, 345, 76, 4, 67];
+// let min = array[0];
+
+// for(i=1; i<array.length; i++){
+//     if (array[i]<min) {
+//         min=array[i];
+//     }
+// }
+// console.log(min);
+
+//2nd Task
+
+let array = [2, 29, 345, 76, 4, 67];
+let max = array[0];
+
+for(i=1; i<array.length; i++){
+    if (array[i]>max) {
+        max=array[i];
+    }
+}
+console.log(max);
