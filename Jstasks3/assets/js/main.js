c5c11dc1c91c8e727b4e6c1a0d7d4775ec5c798e
Elnaraa9 <7khtht32code.edu.az>
2022-11-17 10:36:27 +0400

    task

--- Jstasks3/assets/js/main.js
+++ Jstasks3/assets/js/main.js
@@ -1,23 +1,104 @@
+let array = [2, 29, 345, 76, 4, 67];
+
 // 1st Task
 
-// let array = [2, 29, 345, 76, 4, 67];
 // let min = array[0];
 
-// for(i=1; i<array.length; i++){
-//     if (array[i]<min) {
-//         min=array[i];
+// for (i = 1; i < array.length; i++) {
+//     if (array[i] < min) {
+//         min = array[i];
 //     }
 // }
 // console.log(min);
 
-//2nd Task
+// 2nd Task
 
-let array = [2, 29, 345, 76, 4, 67];
-let max = array[0];
+// let max = array[0];
 
-for(i=1; i<array.length; i++){
-    if (array[i]>max) {
-        max=array[i];
-    }
-}
-console.log(max);
+// for (i = 1; i < array.length; i++) {
+//     if (array[i] > max) {
+//         max = array[i];
+//     }
+// }
+// console.log(max);
+
+// 3rd Task
+
+// let min = array[0];
+
+// for (i = 1; i < array.length; i++) {
+//     if (array[i] < min) {
+//         min = array[i];
+//     }
+// }
+// console.log(array.indexOf(min));
+
+// 4th Task
+
+// let max = array[0];
+
+// for (i = 1; i < array.length; i++) {
+//     if (array[i] > max) {
+//         max = array[i];
+//     }
+// }
+// console.log(array.indexOf(max));
+
+// 5th Task
+
+// let sum = 0;
+// for (i = 1; i < array.length; i++) {
+//     if (i % 2 != 0) {
+//         sum += array[i];
+//     }
+// }
+// console.log(sum);
+
+// 6th Task
+
+// function customReverse(originalArray) {
+
+//     let leftIndex = 0;
+//     let rightIndex = originalArray.length - 1;
+
+//     while (leftIndex < rightIndex) {
+
+//         let temp = originalArray[leftIndex];
+//         originalArray[leftIndex] = originalArray[rightIndex];
+//         originalArray[rightIndex] = temp;
+
+//         leftIndex++;
+//         rightIndex--;
+//     }
+// }
+// customReverse(array);
+// console.log(array);
+
+//7th Task
+
+// let count=0;
+
+// for(i=0; i<array.length;i++){
+//     if (i%2!=0) {
+//         count++;
+//     }
+// }
+// console.log(count);
+
+//8th Task
+
+// let temp;
+// let firstPart = 0;
+// let secondPart = (array.length) / 2;
+
+// while (secondPart<array.length) {
+//     temp=array[firstPart];
+//     array[firstPart]=array[secondPart];
+//     array[secondPart]=temp;
+//     firstPart++;
+//     secondPart++;
+// }
+// console.log(array);
+
+//9th Task
+
